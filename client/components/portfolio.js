3af91571e1330514dde3025dc21bd22190c5f8e1
Claire Watson <watson.e.claire@gmail.com>
2018-11-26 16:35:44 -0500

    adding logic to update total portfolio value on local state, so it can be displayed at the top of portfolio component and update as stock prices change.

--- client/components/portfolio.js
+++ client/components/portfolio.js
@@ -9,7 +9,8 @@ class Portfolio extends Component {
   constructor() {
     super()
     this.state = {
-      prices: {}
+      prices: {},
+      portfolioValue: 0
     }
     this.realTimePrices = this.realTimePrices.bind(this)
     this.fetchPrices = this.fetchPrices.bind(this)
@@ -27,6 +28,7 @@ class Portfolio extends Component {
   async fetchPrices() {
     const portfolio = this.props.portfolio
     let currPrices = {}
+    let portfolioValue = 0
     for (let i = 0; i < portfolio.length; i++) {
       const ticker = portfolio[i].stock.ticker
       const res = await axios.get(
@@ -35,27 +37,36 @@ class Portfolio extends Component {
       const data = res.data
       const open = data.open
       const price = data.iexRealtimePrice
+      const quantity = portfolio[i].quantity
       currPrices[portfolio[i].id] = {open, price}
+      portfolioValue += quantity * price
     }
-    await this.setState({prices: currPrices})
-    console.log('CARROTS')
-    console.log(this.state)
+    await this.setState({prices: currPrices, portfolioValue})
   }
 
   render() {
     const keys = Object.keys(this.state.prices)
-    return (
-      <div>
-        {this.props.portfolio.map(portfolio => {
-          return (
-            <h4 key={portfolio.id}>
-              {portfolio.stock.ticker} - {portfolio.quantity} - ${keys.length
-                ? this.state.prices[portfolio.id].price * portfolio.quantity
-                : null}
-            </h4>
-          )
-        })}
-      </div>
+    return this.props.portfolio.length ? (
+      keys.length ? (
+        <div>
+          <h2>Portfolio: ${this.state.portfolioValue.toLocaleString('en')}</h2>
+          {this.props.portfolio.map(portfolio => {
+            const currentPrice =
+              this.state.prices[portfolio.id].price * portfolio.quantity
+            return (
+              <h4 key={portfolio.id}>
+                {portfolio.stock.ticker} - {portfolio.quantity} - ${currentPrice.toLocaleString(
+                  'en'
+                )}
+              </h4>
+            )
+          })}
+        </div>
+      ) : (
+        <h3>Loading...</h3>
+      )
+    ) : (
+      <h3>You do not have any stocks in your portfolio</h3>
     )
   }
 }
